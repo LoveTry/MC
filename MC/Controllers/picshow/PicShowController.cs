50fd820c631bf6bcb9f7e59a86f1f02431f975b6
LoveTry <linchunbao123@163.com>
2019-03-21 22:10:48 +0800

    layui引入

---
+++ MC/Controllers/picshow/PicShowController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MC.Controllers.picshow
+{
+    public class PicShowController : Controller
+    {
+        // GET: PicShow
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
