0e017be7115e507bd74fa2fff1186859e8cdc25d
LoveTry <linchunbao123@163.com>
2019-04-11 19:20:54 +0800

    css修改

--- MC/App_Start/BundleConfig.cs
+++ MC/App_Start/BundleConfig.cs
@@ -23,8 +23,7 @@ namespace MC
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/pc.css"));
+                      "~/Content/bootstrap.css"));
         }
     }
 }
